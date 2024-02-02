bc7105f0493839a1fa42fec01adb767cd08f7cf3
--Mestevam1976 <88986206+Mestevam1976@users.noreply.github.com>
2024-02-02 09:50:07 -0300

    Alterações visuais no projeto

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ import streamlit as st
 import pandas as pd 
 import numpy as np 
 
-if st.sidebar.button('Exibir Mensagem'):
-    df= pd.DataFrame(
-        np.random.rand(10,3),
-        columns=['Preço', 'Taxa de ocupação', 'Taxa predial']
-    )
-    st.bar_chart(df)
+st.header('Dashboard Streamlit Testes Genéricos', divider='rainbow')
+
+if st.sidebar.button('Exemplo Gráfico de Barras'):
+    st.header('Preço Médio do Litro da Gasolina por Estado (UF) em :blue[2021]')
+    df= pd.read_csv('Csv_files/gasolina_etanol_2021.csv', sep=';')
+    df['Valor de Venda'] = df['Valor de Venda'].replace(',','.',regex=True)
+    df['Valor de Venda'] = pd.to_numeric(df['Valor de Venda'])
+    df1 = df.groupby(by=['Estado - Sigla']).mean(numeric_only=True).reset_index()
+    df1['Valor de Venda'] = df1['Valor de Venda'].round(3)
+    df1.rename(columns={'Estado - Sigla':'UF', 'Valor de Venda':'Valor Médio de Venda'},inplace=True)
+    st.bar_chart(df1[['UF', 'Valor Médio de Venda']], x='UF', y='Valor Médio de Venda', color='#008000', use_container_width=True)
 
 opcao = st.radio(
     'Selecionar uma opcao: ',
